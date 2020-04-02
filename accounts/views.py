0db3a8d67e06412c79ef13c107ce01d1635d2072
Md. Omar Faruk <iamomar022@gmail.com>
2020-04-02 09:43:59 +0600

    Inline Form Sets

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from .forms import OrderForm
+from django.forms import inlineformset_factory ## multiple forms in forms
 
 # Create your views here.
 def home(request):
@@ -45,16 +46,22 @@ def customer(request, pk):
     return render(request, 'accounts/customer.html', context)
 
 
-def createOrder(request):
-    form = OrderForm()
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10) # Parent model and Child model
+    customer = Customer.objects.get(id=pk)
+
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
+
+    # form = OrderForm(initial={'customer':customer}) # To see the customer in form, for which customer profile i viewd.
 
     if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        # form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('home')
 
-    context = {'form':form}
+    context = {'formset':formset}
     return render(request, 'accounts/order_form.html', context)
 
 
