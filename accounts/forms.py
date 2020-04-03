8deb4e70b94692c83b9381c1cc4f0b37f54ba65f
Md. Omar Faruk <iamomar022@gmail.com>
2020-04-03 12:52:59 +0600

    Register and Login Done

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,16 @@
 from django.forms import ModelForm
 from .models import Order
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User   # django default user model
 
 class OrderForm(ModelForm): # name of the model and Form. and inherit from ModelForm.
     class Meta:
         model = Order # which model
         fields = '__all__' # which fields i will allow - ['customer', 'product']. but i take here all.
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
 from django.forms import inlineformset_factory ## multiple forms in forms
 from .filters import OrderFilter
+from django.contrib.auth.forms import UserCreationForm # django default form
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def home(request):
@@ -94,3 +97,41 @@ def deleteOrder(request, pk):
 
     context = {'item':order}
     return render(request, 'accounts/delete.html', context)
+
+
+def registerPage(request):
+    form = CreateUserForm()  # Now UserCreationForm will replace by CreateUserForm
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)  # Now UserCreationForm will replace by CreateUserForm
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username') # To get the user
+            messages.success(request, 'Account was create for ' + user) # I put my user here with message
+
+            return redirect('login')
+
+    context = {'form':form}
+    return render(request, 'accounts/register.html', context)
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password) # to check the user is in model/db or not.
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password is Incorrect')
+
+    context = {}
+    return render(request, 'accounts/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+
+    return redirect('login')
