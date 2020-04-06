7862da83b81755b1e0d531c8fc7792bbc291a894
Md. Omar Faruk <iamomar022@gmail.com>
2020-04-06 08:23:36 +0600

    Conf Done For Reset Pass

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views # for reset password
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -13,4 +14,28 @@ urlpatterns = [
     path('logout/', views.logoutUser, name='logout'),
     path('user/', views.userPage, name='user-page'),
     path('account/', views.accountSettings, name='account'),
+
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/',
+        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"),
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"),
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/',
+        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"),
+        name="password_reset_complete"),
 ]
+
+
+'''
+1 - Submit email form                         //PasswordResetView.as_view()
+2 - Email sent success message                //PasswordResetDoneView.as_view()
+3 - Link to password Rest form in email       //PasswordResetConfirmView.as_view()
+4 - Password successfully changed message     //PasswordResetCompleteView.as_view()
+'''
--- crm/settings.py
+++ crm/settings.py
@@ -128,3 +128,13 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sumaiyabinte580@gmail.com'
+EMAIL_HOST_PASSWORD = 'sumaiya1122334455binte'
